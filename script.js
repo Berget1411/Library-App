41e92d12fc7bc063562c38fa9a762a917dfbc275
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-04 13:15:32 +0200

    add new book object when user submits form
    lets user fill a form and on submit get values of the inputs and creates book object.

--- script.js
+++ script.js
@@ -2,13 +2,37 @@ const addBookButton = document.querySelector(".add-book-button");
 const popUp = document.querySelector(".pop-up");
 const overlay = document.querySelector("#overlay");
 const closeButton = document.querySelector(".pop-up-header button");
+const addBookForm = document.querySelector("form");
 
-addBookButton.addEventListener("click", () => {
+function togglePopup() {
   popUp.classList.toggle("active");
   overlay.classList.toggle("active");
-});
+}
+addBookButton.addEventListener("click", () => togglePopup());
 
-closeButton.addEventListener("click", () => {
-  popUp.classList.toggle("active");
-  overlay.classList.toggle("active");
-});
+closeButton.addEventListener("click", () => togglePopup());
+
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+}
+
+function getValues(e) {
+  e.preventDefault();
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let haveRead = document.querySelector("#have-read").checked;
+  let newBook = new Book(title, author, pages, haveRead);
+  addBookToLibrary(newBook);
+}
+addBookForm.addEventListener("submit", getValues);
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+}
